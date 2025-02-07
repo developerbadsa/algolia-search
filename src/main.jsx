06bdcd82b4ef36113201c60fcc1be7efbc0d290b
Rahim_Badsa <rahimbadsa723@gmail.com>
2025-02-07 12:34:43 +0600

    installed tailwind css and cleared folder and files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <div className='bg-green-500'>ready to start</div>
   </StrictMode>,
 )
